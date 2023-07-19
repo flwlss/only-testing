c0fee9dea22db5c4a7da8e7b8eca86e10381db88
Dmitry <dimaandreev1337@gmail.com>
2023-07-20 00:59:14 +0700

    added scale pagintanion element

--- src/components/MySwiperPagination.tsx
+++ src/components/MySwiperPagination.tsx
@@ -27,12 +27,19 @@ const MySwiperPagination = () => {
 
       for (let i = 0; i < dotsCount; i++) {
         const dot = document.createElement('div');
+        dot.innerHTML = `<p>${i + 1}</p>`
         dot.id = `${i}`;
-        dot.style.width = '6px';
-        dot.style.height = '6px';
+        dot.style.width = '56px';
+        dot.style.height = '56px';
         dot.style.borderRadius = '50%';
         dot.style.backgroundColor = '#42567A';
         dot.style.position = 'absolute';
+        dot.style.cursor = 'pointer';
+        dot.style.color = '#42567A';
+        dot.style.display = 'flex';
+        dot.style.alignItems = 'center';
+        dot.style.justifyContent = 'center';
+        dot.style.fontSize = '20px';
 
         const angle = i * angleStep;
         const x = radius + dotSpacing * Math.cos(angle);
@@ -44,10 +51,21 @@ const MySwiperPagination = () => {
         circle.appendChild(wrapper);
         wrapper.appendChild(dot)
 
+        gsap.to(dot, { scale: 0.1, duration: 0 })
+
         dot.addEventListener('click', () => {
           selectSlide(+dot.id)
-          console.log(dot.id);
         })
+
+        dot.addEventListener("mouseenter",
+          () => gsap.to(dot,
+            { scale: 1, border: '1px solid rgba(48, 62, 88, 0.5)', background: '#F4F5F9' }
+          ));
+
+        dot.addEventListener("mouseleave",
+          () => gsap.to(dot,
+            { scale: 0.1, border: 'none', background: '#42567A' }
+          ));
       }
     }
   }, [swiper]);
