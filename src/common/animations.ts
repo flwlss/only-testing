e4d0f178d6627a9e004238084c136c555f29bcfe
Dmitry <dimaandreev1337@gmail.com>
2023-07-26 19:30:10 +0700

    fix keyEvents

--- src/common/animations.ts
+++ src/common/animations.ts
@@ -5,5 +5,5 @@ export const rotateAnimation = (slideIndex: number) => {
   const rotateDeg = 360 / keyEventsLength * (-slideIndex + 1) - (360 / keyEventsLength - 0.1)
   gsap.to('.circlePagination', { transform: `rotate(${rotateDeg}deg)` })
   gsap.to('.circlePagination__dot', { scale: 0.1, duration: 0, background: '#42567A', transform: `rotate(${-rotateDeg}deg)` })
-  gsap.to(`.circlePagination__dot:nth-child(${slideIndex})`, { scale: 1, border: '1px solid rgba(48, 62, 88, 0.5)', background: '#F4F5F9', duration: 0 })
+  gsap.to(`.circlePagination__dot:nth-child(${slideIndex})`, { scale: 1, border: '1px solid rgba(48, 62, 88, 0.5)', background: '#F4F5F9' })
 }
